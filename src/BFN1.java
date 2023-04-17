e9d105d13842e177ab36cd1a2972d6f90b7edf98
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-04-17 21:42:16 +0200

    name change

---
+++ src/BFN1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class BFN1 {
+    public static void main(String[] args) throws java.lang.Exception {
+        System.out.println("Hello world!");
+    }
+
+    public static int[] Solve(){
+        return new int[]{0, 1};
+    }
+}
